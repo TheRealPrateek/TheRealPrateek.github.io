bb24c0a6957198de3164355f0025f87126645ddc
Prateek Poddar <poddarp312@gmail.com>
2023-07-21 11:31:59 -0400

    added hamburger menu

---
+++ public/js/scripts.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const menuIcon = document.querySelector(".hburgmenu .fa-bars");
+
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    menuIcon.style.display = "none";
+  }
+};
